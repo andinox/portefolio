9a4561a8b1d8794845523eef74b1769b67497ba7
andinox <56691495+andinox@users.noreply.github.com>
2022-03-19 11:08:28 +0100

    news

--- script.js
+++ script.js
@@ -8,6 +8,24 @@ function getRandomArbitraryInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 100;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+reveal();
+
 function display_load() {
     const loader = document.getElementById("loader");
     load = (load > 98) ? 100 : load;
